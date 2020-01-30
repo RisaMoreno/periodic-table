d1151386b0eda28ae0bfe0a68747bf95a0ad8c06
haben <haben317@gmail.com>
2020-01-30 15:55:00 -0800

    added atomicData

--- periodic_table/urls.py
+++ periodic_table/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('atom/<int:atomic_num>', views.show_atom),
 ]
--- periodic_table/views.py
+++ periodic_table/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
+from atomicData import data
+all_atoms = json.loads(data)
 
 def index(request):
     return render(request, "index.html")
+
+def show_atom(request, atomic_num):
+    print(atomic_num)
+    print(all_atoms[int(atomic_num)])
+    return redirect('/')
